d4f33f843dc4d943ada0dc686e2e690aafdc9a39
heaganhenry <heaganhenry@gmail.com>
2022-11-14 18:52:15 -0400

    Update reminders route with POST request

--- routes/reminders.ts
+++ routes/reminders.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import Reminder from '../models/reminder';
+import HasTitle from '../interfaces/HasTitle';
 
 const router = express.Router();
+const reminders: Reminder[] = [];
 
 router.get('/', (req, res) => {
-    res.status(201).json({ msg: 'List of all reminders' });
+    res.status(200).json(reminders);
+});
+
+router.post('/', (req, res) => {
+    const { title } = req.body as HasTitle;
+    const reminder = new Reminder(title);
+    reminders.push(reminder);
+    res.status(201).json(reminder);
 });
 
 export default router;
